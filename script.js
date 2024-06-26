0dc5d877c6269e1b327260ecb665060408b86b4f
oAmorim2k5 <victorhuski@gmail.com>
2024-06-26 15:58:24 -0300

    HTML and JS changes, starting with javascript

---
+++ script.js
@@ -0,0 +1,31 @@
+let bill = 0
+let people = 0
+let tip = 0
+
+const billInput = document.querySelector("#bill")
+billInput.addEventListener("input", receiveBillValue)
+
+function receiveBillValue(event) {
+    bill = Number(event.target.value)
+    console.log("Account value: " + bill)
+}
+
+const peopleInput = document.querySelector("#people")
+peopleInput.addEventListener("input", receivePeopleQuant )
+
+function receivePeopleQuant(event){
+    let divError = document.querySelector(".people .input-box")
+    let pError = document.querySelector(".people #error")
+    if(event.target.value === "0"){
+        pError.style.display = "block"
+        divError.setAttribute("id", "error-div")
+        console.log("Error")
+    } else {
+        people = Number(event.target.value)
+        pError.style.display = "none"
+        divError.setAttribute("id", "")
+    }
+    people = Number(event.target.value)
+    console.log("Number of people: " + people)
+
+}
